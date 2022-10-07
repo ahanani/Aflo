702a5367ed83f673459031e5c02f3a606f0082ae
simron-adiwal <sadiwal@my.bcit.ca>
2022-10-07 14:36:51 -0700

    initialized my files for Lab 5

---
+++ app/src/main/java/com/example/aflo/FlightTypeSelection.java
@@ -0,0 +1,14 @@
+package com.example.aflo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlightTypeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_type_selection);
+    }
+}
