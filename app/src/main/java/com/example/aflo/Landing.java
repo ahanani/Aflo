5677a8c73287f9768a06af3d9c58b388ac3d9532
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-10-12 14:51:29 -0700

    Added strings

--- app/src/main/java/com/example/aflo/Landing.java
+++ app/src/main/java/com/example/aflo/Landing.java
@@ -1,4 +1,14 @@
 package com.example.aflo;
 
-public class Landing {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Landing extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
 }
