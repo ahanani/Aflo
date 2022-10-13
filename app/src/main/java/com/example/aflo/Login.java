d6c9faf34a4fb715dcbb9c4cc643671c28624ce0
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-10-13 16:10:49 -0700

    Create login btn handler

--- app/src/main/java/com/example/aflo/Login.java
+++ app/src/main/java/com/example/aflo/Login.java
@@ -1,6 +1,8 @@
 package com.example.aflo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        createButtons();
+    }
+
+    public void createButtons() {
+        Button loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(view -> {
+            Intent loginAndRedirectToHome = new Intent(view.getContext(), Landing.class);
+            startActivity(loginAndRedirectToHome);
+        });
     }
 }
