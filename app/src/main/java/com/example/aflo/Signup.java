218c8d9c1df4e650fb586c07d0db49f41f8fad0a
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-10-13 16:23:16 -0700

    Created new Signup activity

---
+++ app/src/main/java/com/example/aflo/Signup.java
@@ -0,0 +1,14 @@
+package com.example.aflo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signup extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
