a363747bf710dea9b7eebfdf9279def38d5a8d24
simron-adiwal <sadiwal@my.bcit.ca>
2022-10-14 13:37:20 -0700

    implement departing flight, departing flight details, and flight type

--- app/src/main/java/com/example/aflo/FlightDepartingSelection.java
+++ app/src/main/java/com/example/aflo/FlightDepartingSelection.java
@@ -1,12 +1,18 @@
 package com.example.aflo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FlightDepartingSelection extends AppCompatActivity {
@@ -16,12 +22,34 @@ public class FlightDepartingSelection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_departing_selection);
 
-        String s= "Select your departing flight (MM/DD/YYYY - AIRPORT_CODE)";
+        // top text for dates
+        String s= "Select your departing flight (Oct 18, 2022 - YVR)";
         SpannableString ss1=  new SpannableString(s);
         ss1.setSpan(new RelativeSizeSpan(1.5f), 0,28, 0); // set size
-        ss1.setSpan(new ForegroundColorSpan(Color.RED), 0, 28, 0);// set color
+        ss1.setSpan(new ForegroundColorSpan(Color.WHITE), 0, s.length(), 0);// set color
         TextView tv= (TextView) findViewById(R.id.select_your_departing_flight_textview);
         tv.setText(ss1);
+
+
+        // fragments --> flight selections
+        Fragment selection = new flight_departing_selection();
+        Fragment details = new FlightDepartingSelectionDetails();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, selection);
+        fragmentTransaction.commit();
+
+
+//        ConstraintLayout firstSelection = findViewById(R.id.firstChoice);
+//        firstSelection.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentTransaction fragmentTransaction2 = fragmentManager.beginTransaction();
+//                fragmentTransaction2.replace(R.id.fragment, details);
+//                fragmentTransaction2.commit();
+//            }
+//        });
 
 
 
