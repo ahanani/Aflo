70cf48f8e6e0bc9176b1344209d2b2ab0fcfd9d6
ahanani <ahanani@my.bcit.ca>
2022-10-14 15:19:05 -0700

    small changes

--- app/src/main/java/com/example/aflo/HotelInformation.java
+++ app/src/main/java/com/example/aflo/HotelInformation.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class HotelInformation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
