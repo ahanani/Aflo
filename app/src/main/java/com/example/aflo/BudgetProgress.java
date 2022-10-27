f859760c93c8b0cbaf1c3c32a48230bec177d088
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-10-27 16:41:57 -0700

    Added progressbar to hotel page

--- app/src/main/java/com/example/aflo/BudgetProgress.java
+++ app/src/main/java/com/example/aflo/BudgetProgress.java
@@ -3,5 +3,7 @@ package com.example.aflo;
 import androidx.fragment.app.Fragment;
 
 public class BudgetProgress extends Fragment {
-
+    public BudgetProgress() {
+        super(R.layout.fragment_progress_bar);
+    }
 }
