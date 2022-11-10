011029c24e33d971486f0c287212447acd75e6fe
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-11-09 19:00:09 -0800

    Implemented login

--- app/src/main/java/com/example/aflo/Login.java
+++ app/src/main/java/com/example/aflo/Login.java
@@ -2,18 +2,29 @@ package com.example.aflo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         createButtons();
     }
@@ -31,11 +42,27 @@ public class Login extends AppCompatActivity {
         String emailString = email.getText().toString();
         String passwordString = password.getText().toString();
 
-        if (validate(emailString, passwordString)) {
-            Intent loginAndRedirectToHome = new Intent(this, MainMenu.class);
-            startActivity(loginAndRedirectToHome);
+        if (!validate(emailString, passwordString)) {
+            return;
         }
 
+        mAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("auth", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Intent loginAndRedirectToHome = new Intent(
+                                Login.this, MainMenu.class);
+                        startActivity(loginAndRedirectToHome);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("auth", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(Login.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public boolean validate(String email, String password) {
