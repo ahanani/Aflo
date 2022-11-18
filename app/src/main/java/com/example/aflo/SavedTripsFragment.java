368d1a611ee329c22488112a25dafae0007343cc
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-11-18 10:52:25 -0800

    Added recycler to fragment container

--- app/src/main/java/com/example/aflo/SavedTripsFragment.java
+++ app/src/main/java/com/example/aflo/SavedTripsFragment.java
@@ -1,23 +1,9 @@
 package com.example.aflo;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SavedTripsFragment extends Fragment {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public SavedTripsFragment() {
+        super(R.layout.fragment_saved_trips);
     }
 }
