89ec3574cff9b7a6596e77c99932dd1f66804296
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-11-18 09:58:50 -0800

    Implemented RecyclerView

--- app/src/main/java/com/example/aflo/SavedTripsRecycler.java
+++ app/src/main/java/com/example/aflo/SavedTripsRecycler.java
@@ -1,32 +1,57 @@
 package com.example.aflo;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Context;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class SavedTripsRecycler extends RecyclerView.Adapter<SavedTripsRecycler.SavedTripsHolder> {
+    private Context context;
+    private ArrayList<Object> savedTrips;
+
+    public SavedTripsRecycler(Context c, ArrayList<Object> trip) {
+        //TODO: Change trip type Object to type Trip class
+        context = c;
+        savedTrips = trip;
+    }
+
     @NonNull
     @Override
     public SavedTripsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View tripView = inflater.inflate(R.layout.fragment_saved_trip, parent, false);
+        return new SavedTripsHolder(tripView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SavedTripsHolder holder, int position) {
+        //TODO: Change trip type Object to type Trip class
+        Object trip = savedTrips.get(position);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return savedTrips.size();
     }
 
     public class SavedTripsHolder extends RecyclerView.ViewHolder {
+        TextView fromDate, toDate, origin, destination, total;
 
         public SavedTripsHolder(@NonNull View itemView) {
             super(itemView);
+            fromDate = itemView.findViewById(R.id.dateAVS);
+            toDate = itemView.findViewById(R.id.dateBVS);
+            origin = itemView.findViewById(R.id.originV);
+            destination = itemView.findViewById(R.id.destinationV);
+            total = itemView.findViewById(R.id.budgetV);
         }
     }
 }
