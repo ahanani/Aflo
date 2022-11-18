139c0feeb1ed416ff74ccabe413a91e888c5056e
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-11-17 18:32:21 -0800

    initial commit

---
+++ app/src/main/java/com/example/aflo/SavedTripsActivity.java
@@ -0,0 +1,14 @@
+package com.example.aflo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SavedTripsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_trips);
+    }
+}
