6e9034e7504f35e597875503adf428bd2d4af05c
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-11-25 16:37:20 -0800

    initial commit

---
+++ app/src/main/java/com/example/aflo/FlightPackage.java
@@ -0,0 +1,29 @@
+package com.example.aflo;
+
+public class FlightPackage {
+    private String uri;
+    private String linkBody;
+    private String outboundId;
+    private String inboundId;
+
+    private String outboundDate;
+    private String outboundTime;
+    private String inboundDate;
+    private String inboundTime;
+    private double price;
+
+    // Expanded
+    private String outboundCarrier;
+    private String outboundCity;
+    private String outboundAirportCode;
+    private String inboundCarrier;
+    private String inboundCity;
+    private String inboundAirportCode;
+    private String deeplink;
+
+    // Constructor
+    // Constructor Expanded
+    // toBundle
+    // get
+    // set
+}
